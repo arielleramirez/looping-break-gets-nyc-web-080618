424b274381f8805e8e5e4b181d1575489f8960ba
Arielle N Ramirez <ariellenramirez@gmail.com>
2018-07-25 14:39:10 +0000

    automatically backed up by learn

--- levitation_quiz.rb
+++ levitation_quiz.rb
@@ -1,14 +1,11 @@
 
 def levitation_quiz
-	counter = 1
- 
-while counter <= 5
-  puts "What is the spell that enacts levitation?"
-  break if counter == 3
-  counter = counter + 1
-end
- 
-puts "We're done looping!"
+	  loop do 
+        puts "What's up, Doc?"
+        answer = gets.chomp
+        break if answer == "STOP"
+    end
+    puts "Okay, okay, jeez. I'll stop. Sorry."
 end
 
 
