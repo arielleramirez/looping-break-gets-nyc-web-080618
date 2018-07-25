54c6b5617d64ac988e1d56277da816a12e0cbb2c
Arielle N Ramirez <ariellenramirez@gmail.com>
2018-07-25 14:39:21 +0000

    automatically backed up by learn

--- levitation_quiz.rb
+++ levitation_quiz.rb
@@ -3,7 +3,7 @@ def levitation_quiz
 	  loop do 
         puts "What's up, Doc?"
         answer = gets.chomp
-        break if answer == "STOP"
+        break if answer == "Wingardium Leviosa"
     end
     puts "Okay, okay, jeez. I'll stop. Sorry."
 end
